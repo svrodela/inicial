b8f0108baaa3df71e8bc6fd2daacc71d095b7073
svrodela <svrodela@gmail.com>
2022-09-20 15:52:54 -0500

    inserción de campos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const router = express.Router();
+//const model = require('../model/valores')();
+
+const Valor = require('../model/valores');
 
 router.get('/',(req, res)=>{
     res.render('index',{
        nombre:'Sergio' 
     })
+});
+
+router.post('/add', async (req, res) => {
+    const valor = new Valor(req.body);
+    await valor.save();
+    res.redirect('/');
 });
 
 module.exports = router;
